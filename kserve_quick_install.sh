1c2b08f1e4ecd171f5d2bfe360c99d97fdd9bccb
lauramariel <lauram93@gmail.com>
2024-02-28 15:36:02 -0800

    --short=true deprecated: https://github.com/kubernetes/kubernetes/issues/122455

--- kserve_quick_install.sh
+++ kserve_quick_install.sh
@@ -37,7 +37,7 @@ export KSERVE_VERSION=v0.11.1
 export CERT_MANAGER_VERSION=v1.3.0
 export SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
 
-KUBE_VERSION=$(kubectl version --short=true | grep "Server Version" | awk -F '.' '{print $2}')
+KUBE_VERSION=$(kubectl version | grep "Server Version" | awk -F '.' '{print $2}')
 if [ ${KUBE_VERSION} -lt 24 ];
 then
    echo "😱 install requires at least Kubernetes 1.24";
